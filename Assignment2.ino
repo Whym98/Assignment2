ed2dd38555a36a68e31f15b2937b2318e8b80d03
Whym98 <98490406+Whym98@users.noreply.github.com>
2022-03-09 18:33:27 +0000

    Add files via upload

--- Assignment2.ino
+++ Assignment2.ino
@@ -7,6 +7,9 @@ volatile int count = 0;
 volatile int avecount = 0;
 volatile int POTval[4] = {0, 0, 0, 0};
 volatile int POTvalave = 0;
+volatile byte BUTTONSTATE = 0;
+volatile byte error_code = 0;
+volatile double SIGFREQ = 0;
 
 void setup() 
 {
@@ -24,53 +27,100 @@ void setup()
 
 void loop() 
 {
-  if((count % 42)) == 0)
+  if((count % 42) == 0)
   {
     ADC();
     ADCAVE();
   }
-  if((count % 200)) == 0)
+  if((count % 200) == 0)
   {
     BUTTONREAD();
   }
-  if((count % 100)) == 0)
+  if((count % 100) == 0)
   {
     ASM();
   }
-  if((count % 1000)) == 0)
+  if((count % 1000) == 0)
   {
     SIGREAD();
   }
-  if((count % 333)) == 0)
+  if((count % 333) == 0)
   {
     ERRORCALC();
     ERRORLED();
   }
-  if((count % 5000)) == 0)
+  if((count % 5000) == 0)
   {
-    SERIAL()
+    SERIALPRINT();
   }
-  delayMicroseconds(1):
+  delayMicroseconds(1);
   count++;
 }
 
-ADC()
+void ADC()
 {
   POTval[avecount] = analogRead(POT);
-}
-
-ADCAVE()
-{
-  int loop;
-  int TOT = 0;
   avecount++;
-  if(avecount >= 4) 
+  if(avecount >= 3) 
   {
     avecount = 0;
   }
+}
+
+void ADCAVE()
+{
+  int loop;
+  int TOT = 0;
   for(loop = 0; loop < 4; loop++) //add up last 4 readings
   {
       TOT = TOT + POTval[loop];
   }
-  POTvalave = avep / 4; //find average of readings and save to global variable 
+  POTvalave = TOT / 4; //find average of readings and save to global variable 
 } 
+
+void BUTTONREAD()
+{
+  BUTTONSTATE = digitalRead(BUTTON);
+}
+
+void ASM()
+{
+  int i;
+  for(i = 0; i < 1000; i++)
+  {
+    __asm__ __volatile__ ("nop");
+  }
+}
+
+void SIGREAD()
+{
+  int pulsetime = 0;
+  pulsetime = pulseIn(SIG, HIGH, 2500);
+  SIGFREQ = 1000000 / pulsetime;
+}
+
+void ERRORCALC()
+{
+  if(POTvalave > 2048)
+  {
+    error_code = 1;
+  }
+  else
+  {
+    error_code = 0;
+  }
+}
+
+void ERRORLED()
+{
+  digitalWrite(ERROR, error_code);
+}
+
+void SERIALPRINT()
+{
+  Serial.print(BUTTONSTATE);
+  Serial.print(",");
+  Serial.print(SIGFREQ);
+  Serial.print(",");
+  Serial.println(POTvalave);
+}
